317338ef654fce47022f573a7f8372d389519620
Aldo Fiore <aldo.fiore95@gmail.com>
2023-12-01 09:43:19 +0100

    day 1 a done

--- day-1/day-1-a.ts
+++ day-1/day-1-a.ts
@@ -1,8 +1,24 @@
 // input contains the content of the input file
 // you choose to use
 export default function (input: string) {
-  console.log('input:', input)
-  console.log('                         ')
-  // your code goes here
-  console.log('result:', undefined)
+  console.log("input:", input);
+  console.log("                         ");
+
+  const sequences = input.split("\n");
+
+  let result = 0;
+
+  sequences.forEach((sequence) => {
+    // you can use regex to do this, I tried but I was raging
+    const allNumbersInSequence = sequence
+      .split("")
+      .filter((char) => !isNaN(parseInt(char)));
+    const usableNumber = parseInt(
+      `${allNumbersInSequence?.at(0) || 0}${allNumbersInSequence?.at(-1) || 0}`
+    );
+
+    result += usableNumber;
+  });
+
+  console.log("result:", result);
 }
