e7b2a6a1f49e0719065f1c16cec613b1d86c1096
Aldo Fiore <aldo.fiore95@gmail.com>
2023-12-01 11:15:47 +0100

    part 2 done

--- day-1/day-1-b.ts
+++ day-1/day-1-b.ts
@@ -1,8 +1,76 @@
 // input contains the content of the input file
 // you choose to use
 export default function (input: string) {
-  console.log('input:', input)
-  console.log('                         ')
-  // your code goes here
-  console.log('result:', undefined)
+  console.log("input:", input);
+  console.log("                         ");
+
+  const sequences = input.split("\n");
+
+  let result = 0;
+
+  const numbersInDigitToNumberMap: Record<string, string> = {
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+  };
+
+  sequences.forEach((sequence) => {
+    const characters = sequence.split("");
+    let firstNumber = "0";
+    let secondNumber = "0";
+
+    for (let i = 0; i < characters.length; i++) {
+      const character = characters[i];
+
+      if (!isNaN(parseInt(character))) {
+        firstNumber = character;
+        break;
+      }
+
+      Object.keys(numbersInDigitToNumberMap).forEach((wordNumber) => {
+        const possibleNumberWord = sequence.substring(i, i + wordNumber.length);
+
+        if (possibleNumberWord === wordNumber) {
+          firstNumber = numbersInDigitToNumberMap[wordNumber];
+        }
+      });
+
+      if (firstNumber !== "0") {
+        break;
+      }
+    }
+
+    for (let i = characters.length - 1; i >= 0; i--) {
+      const character = characters[i];
+
+      if (!isNaN(parseInt(character))) {
+        secondNumber = character;
+        break;
+      }
+
+      Object.keys(numbersInDigitToNumberMap).forEach((wordNumber) => {
+        const possibleNumberWord = sequence.substring(i - wordNumber.length + 1, i + 1);
+
+        if (possibleNumberWord === wordNumber) {
+          secondNumber = numbersInDigitToNumberMap[wordNumber];
+        }
+      });
+
+      if (secondNumber !== "0") {
+        break;
+      }
+    }
+
+    const usableNumber = parseInt(`${firstNumber}${secondNumber}`);
+
+    result += usableNumber;
+  });
+
+  console.log("result:", result);
 }
