ba034f371269c51c057069038595c00ff72070bd
Aldo Fiore <aldo.fiore95@gmail.com>
2023-12-02 16:25:10 +0100

    day 1 a better version

--- day-2/day-2-a.ts
+++ day-2/day-2-a.ts
@@ -10,29 +10,28 @@ export default function (input: string) {
     blue: 14,
   };
 
-  const games = input.split("\n").map((game) => game.replace(/\s+/g, ""));
+  const result = input
+    .split("\n")
+    .map((game) => game.replace(/\s+/g, ""))
+    .map((game) => {
+      const [_, cubesShown] = game.split(":");
 
-  let result = 0;
+      return cubesShown.split(";").map((cubes) => {
+        const singleColor = cubes.split(",");
 
-  for (const game of games) {
-    const [gameInfo, cubesShown] = game.split(":");
+        return singleColor.reduce((isPossible, color) => {
+          const amount = color.match(/\d+/)?.at(0);
+          const colorName = color.substring(amount?.length || 0);
 
-    const possibleGame = cubesShown.split(";").map((cubes) => {
-      const singleColor = cubes.split(",");
-
-      return singleColor.reduce((isPossible, color) => {
-        const amount = color.match(/\d+/)?.at(0);
-        const colorName = color.substring(amount?.length || 0);
-
-        return isPossible && parseInt(amount || "0") <= MAX[colorName];
-      }, true);
-    });
-
-    if (possibleGame.every((isPossible) => isPossible)) {
-      const gameIdNumber = gameInfo.match(/\d+/)?.at(0);
-      result += parseInt(gameIdNumber || "0");
-    }
-  }
+          return isPossible && parseInt(amount || "0") <= MAX[colorName];
+        }, true);
+      });
+    })
+    .reduce(
+      (sum, allPullResults, i) =>
+        allPullResults.every(Boolean) ? sum + i + 1 : sum,
+      0
+    );
 
   console.log("result:", result);
 }
