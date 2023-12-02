18086c0671627d39f1577f6319868bf57abd3e53
Aldo Fiore <aldo.fiore95@gmail.com>
2023-12-02 15:26:52 +0100

    day 2 b done

--- day-2/day-2-b.ts
+++ day-2/day-2-b.ts
@@ -3,6 +3,34 @@
 export default function (input: string) {
   console.log('input:', input)
   console.log('                         ')
-  // your code goes here
-  console.log('result:', undefined)
+
+  const games = input.split("\n").map((game) => game.replace(/\s+/g, ""));
+
+  let result = 0;
+
+  for (const game of games) {
+    const power: Record<string, number> = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+
+    const [_, cubesShown] = game.split(":");
+
+    cubesShown.split(";").forEach((cubes) => {
+      const singleColor = cubes.split(",");
+
+      singleColor.forEach((color) => {
+        const amount = color.match(/\d+/)?.at(0);
+        const colorName = color.substring(amount?.length || 0);
+        const amountNumber = parseInt(amount || "0");
+
+        power[colorName] = amountNumber > power[colorName] ? amountNumber : power[colorName];
+      });
+    });
+
+    result += Object.values(power).reduce((sum, value) => sum * value);
+  }
+
+  console.log('result:', result)
 }
